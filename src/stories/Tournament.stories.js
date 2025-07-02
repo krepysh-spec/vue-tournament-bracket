119a3ec51d6907100fcfd94f9c64870981f65f57
ymykhailichenko <ymykhailichenko@neklo.com>
2025-07-02 13:17:35 +0300

    Add storybook

---
+++ src/stories/Tournament.stories.js
@@ -0,0 +1,55 @@
+import { fn } from 'storybook/test';
+
+import Tournament from './Tournament.vue';
+import {PERMISSIONS, TOURNAMENT_FORMAT} from '../constants/tournament';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+export default {
+  title: 'Tournament',
+  component: Tournament,
+  tags: ['autodocs'],
+  argTypes: {
+    theme: {
+      control: { type: 'select' },
+      options: ['dark', 'lite'],
+    },
+    format: {
+      control: { type: 'select' },
+      options: [TOURNAMENT_FORMAT.SINGLE_ELIMINATION, TOURNAMENT_FORMAT.DOUBLE_ELIMINATION],
+    },
+    size: {
+      control: { type: 'select' },
+      options: [2, 4, 8, 16, 32, 64],
+    },
+    defaultBestOf: {
+      control: { type: 'select' },
+      options: [1, 3, 5, 7, 9],
+    },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {
+    format: TOURNAMENT_FORMAT.SINGLE_ELIMINATION,
+    size: 16,
+    defaultBestOf: 3,
+    permissions: {
+      [PERMISSIONS.CAN_SELECT_TEAM]: true,
+      [PERMISSIONS.CAN_EDIT_DATE]: true,
+      [PERMISSIONS.CAN_EDIT_SCOPE]: true,
+      [PERMISSIONS.CAN_EDIT_ROUND_NAME]: true,
+      [PERMISSIONS.CAN_EDIT_BEST_OF]: true,
+    },
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const SingleElimination = {
+  args: {
+    format: TOURNAMENT_FORMAT.SINGLE_ELIMINATION,
+  },
+};
+
+export const DoubleElimination = {
+  args: {
+    format: TOURNAMENT_FORMAT.DOUBLE_ELIMINATION,
+  },
+};
---
+++ src/stories/Tournament.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from 'vue';
+import "../assets/css/style.css"
+import TournamentBracket from "../components/TournamentBracket.vue";
+import { createTournamentState } from '../utils/tournament';
+
+const props = defineProps({
+  size: {
+    type: Number,
+    required: 16,
+  },
+  defaultBestOf: {
+    type: Number,
+    default: 1,
+  },
+  format: {
+    type: String,
+    // validator: function (value) {
+    //   return ['small', 'medium', 'large'].indexOf(value) !== -1;
+    // },
+  },
+})
+
+
+const teams = ref([
+  {id: 1, name: 'Alpha', logo: 'https://www.gravatar.com/avatar/1?d=identicon&s=32'},
+  {id: 2, name: 'Beta', logo: 'https://www.gravatar.com/avatar/2?d=identicon&s=32'},
+  {id: 3, name: 'Gamma', logo: 'https://www.gravatar.com/avatar/3?d=identicon&s=32'},
+  {id: 4, name: 'Delta', logo: 'https://www.gravatar.com/avatar/4?d=identicon&s=32'},
+  {id: 5, name: 'Epsilon', logo: 'https://www.gravatar.com/avatar/5?d=identicon&s=32'},
+  {id: 6, name: 'Zeta', logo: 'https://www.gravatar.com/avatar/6?d=identicon&s=32'},
+  {id: 7, name: 'Eta', logo: 'https://www.gravatar.com/avatar/7?d=identicon&s=32'},
+  {id: 8, name: 'Theta', logo: 'https://www.gravatar.com/avatar/8?d=identicon&s=32'},
+  {id: 9, name: 'Iota', logo: 'https://www.gravatar.com/avatar/9?d=identicon&s=32'},
+  {id: 10, name: 'Kappa', logo: 'https://www.gravatar.com/avatar/10?d=identicon&s=32'},
+  {id: 11, name: 'Lambda', logo: 'https://www.gravatar.com/avatar/11?d=identicon&s=32'},
+  {id: 12, name: 'Mu', logo: 'https://www.gravatar.com/avatar/12?d=identicon&s=32'},
+  {id: 13, name: 'Nu', logo: 'https://www.gravatar.com/avatar/13?d=identicon&s=32'},
+  {id: 14, name: 'Xi', logo: 'https://www.gravatar.com/avatar/14?d=identicon&s=32'},
+  {id: 15, name: 'Omicron', logo: 'https://www.gravatar.com/avatar/15?d=identicon&s=32'},
+  {id: 16, name: 'Pi', logo: 'https://www.gravatar.com/avatar/16?d=identicon&s=32'},
+]);
+
+
+const tournamentState = ref(createTournamentState(props.size.value, props.defaultBestOf.value));
+
+</script>
+
+<template>
+  <tournament-bracket
+      :format="format"
+      :default-best-of="3"
+      :available-teams="teams"
+      :initial-state="tournamentState"/>
+</template>
+
+<style scoped>
+
+</style>
