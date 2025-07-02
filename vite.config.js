9a6e5b477dfdb05462de2f3a525e85944942917e
ymykhailichenko <ymykhailichenko@neklo.com>
2025-07-02 12:01:13 +0300

    Add storybook

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
